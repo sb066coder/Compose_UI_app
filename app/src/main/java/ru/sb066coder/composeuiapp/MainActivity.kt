9039903bb623d62f30c9d89d3307a9e8345a845d
Sergey Buksha <sergey066@yandex.ru>
2023-12-15 14:40:41 +0300

    Unused import removed

--- app/src/main/java/ru/sb066coder/composeuiapp/MainActivity.kt
+++ app/src/main/java/ru/sb066coder/composeuiapp/MainActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
