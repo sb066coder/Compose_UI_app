f93abb884a03b6bb59c9548aa1edbc1b0d1b33e3
Sergey Buksha <sergey066@yandex.ru>
2023-12-15 18:09:40 +0300

    Quadrants complete

--- app/src/main/java/ru/sb066coder/composeuiapp/MainActivity.kt
+++ app/src/main/java/ru/sb066coder/composeuiapp/MainActivity.kt
@@ -3,9 +3,10 @@ package ru.sb066coder.composeuiapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -14,12 +15,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import ru.sb066coder.composeuiapp.ui.theme.ComposeUIAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,13 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier
-                    ) {
-                        OkGroup()
-                    }
+                    Quadrants()
                 }
             }
         }
@@ -46,36 +41,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun OkGroup(modifier: Modifier = Modifier) {
-    val image = painterResource(id = R.drawable.ic_task_completed)
-    Column {
-        Image(
-            modifier = modifier,
-            painter = image,
-            contentDescription = "OK icon"
-        )
+fun Quadrants() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.weight(1f)) {
+            Card(
+                title = stringResource(R.string.title1),
+                content = stringResource(R.string.description1),
+                color = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            Card(
+                title = stringResource(R.string.title2),
+                content = stringResource(R.string.description2),
+                color = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            Card(
+                title = stringResource(R.string.title3),
+                content = stringResource(R.string.description3),
+                color = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            Card(
+                title = stringResource(R.string.title4),
+                content = stringResource(R.string.description4),
+                color = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun Card(
+    title: String,
+    content: String,
+    color: Color, modifier:
+    Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = color)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
-            text = stringResource(R.string.all_tasks_completed),
+            text = title,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .padding(
-                    top = 24.dp,
-                    bottom = 8.dp
-                )
-                .align(Alignment.CenterHorizontally)
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
-            text = stringResource(R.string.nice_work),
-            fontSize = 16.sp,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+            text = content,
+            textAlign = TextAlign.Justify
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun TaskManagerPreview() {
+fun QuadrantsPreview() {
     ComposeUIAppTheme {
-        OkGroup()
+        Quadrants()
     }
 }
